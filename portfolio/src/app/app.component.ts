3931a57f31286cabfa33769a57ad00648b09b5ef
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-01-19 18:34:58 +0100

    atf subleties, intro section preps

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AtfComponent } from './atf/atf.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AtfComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, AtfComponent, HeaderComponent, IntroComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ portfolio/src/app/intro/intro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SkillsComponent } from './skills/skills.component';
+@Component({
+  selector: 'app-intro',
+  standalone: true,
+  imports: [SkillsComponent],
+  templateUrl: './intro.component.html',
+  styleUrl: './intro.component.scss'
+})
+export class IntroComponent {
+
+}
