2a503bb7956d62b217b4727847ed4e69b48ed904
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-02-15 23:12:49 +0100

    nav adjustm. / intro btn adjustm.

--- portfolio/src/app/intro/intro.component.ts
+++ portfolio/src/app/intro/intro.component.ts
@@ -9,5 +9,28 @@ import { SkillsComponent } from './skills/skills.component';
 })
 export class IntroComponent {
 
- 
+  private originalImages: { [key: string]: string } = {
+    location: '../../assets/img/location.png',
+    remote: '../../assets/img/remote.png',
+    move: '../../assets/img/move.png'
+  };
+
+  private hoverImages: { [key: string]: string } = {
+    location: '../../assets/img/location-hover.png',
+    remote: '../../assets/img/remote-hover.png',
+    move: '../../assets/img/move-hover.png'
+  };
+
+  introChangeImg(event: Event, isHover: boolean) {
+    const imgElement = event.target as HTMLImageElement;
+    if (!imgElement) return;
+
+    const imgKey = Object.keys(this.originalImages).find(key => 
+      imgElement.src.includes(this.originalImages[key].split('/').pop()!)
+    );
+
+    if (imgKey) {
+      imgElement.src = isHover ? this.hoverImages[imgKey] : this.originalImages[imgKey];
+    }
+  }
 }
