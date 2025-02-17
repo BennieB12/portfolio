20d920d056bf351858b349ab23229bbb5906c3e6
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-02-17 16:26:13 +0100

    intro section btn styling, icon positioning / project section background styling

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './intro/intro.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AtfComponent, HeaderComponent, IntroComponent ,SkillsComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, AtfComponent, HeaderComponent, IntroComponent ,SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
