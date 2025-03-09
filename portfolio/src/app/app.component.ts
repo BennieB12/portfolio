0b17f2a7c8cb6302a7be65852c1c9ad81555c766
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-03-09 19:57:44 +0100

    added custom-scrollbar and ngx-translate

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { TranslateModule } from "@ngx-translate/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -11,11 +13,27 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     CommonModule,
     RouterOutlet,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TranslateModule
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  // constructor(private translate: TranslateService) {
+  //   this.translate.addLangs(['de', 'en']);
+  //   this.translate.setDefaultLang('en');
+  //   this.translate.use('en');
+  // }
+
+  //  useLanguage(event: Event, language: string): void {
+  //   event.preventDefault();
+  //   this.translate.use(language);
+  // }
+
+  // get currentLanguage(): string {
+  //   return this.translate.currentLang;
+  // }
 }
