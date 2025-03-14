b5e20423c8fac25a3187c8c7140c7c29ec9751c0
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-03-14 18:53:18 +0100

    more bubbly bubbles

--- portfolio/src/app/main-content/atf/atf.component.ts
+++ portfolio/src/app/main-content/atf/atf.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { fadeIn } from '../../animations/animations';
+import { fadeIn} from '../../animations/animations';
 
 @Component({
   selector: 'app-atf',
