23d9b96e4f5b7d3c1cdc265e8453c8628c56f466
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-03-15 05:06:57 +0100

    bubblx bubble animations + positioning

---
+++ portfolio/src/app/main-content/bubbles/bubbles.component.ts
@@ -0,0 +1,108 @@
+import { Component, HostListener, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-effect',
+  standalone: true,
+  templateUrl: './bubbles.component.html',
+  styleUrls: ['./bubbles.component.scss']
+})
+export class BubbleEffectComponent implements OnInit, AfterViewInit {
+  bubbliness = 3; // Menge an Bubbles: 1 - 10
+  motionResistance = 3; // Bewegungsträgheit: 1 - 10
+
+  private layer!: HTMLElement;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.initializeBubbles();
+  }
+
+  initializeBubbles(): void {
+
+    if (this.layer) {
+      this.layer.remove();
+    }
+    this.layer = this.renderer.createElement('div');
+    this.renderer.addClass(this.layer, 'layer');
+    this.renderer.appendChild(document.body, this.layer);
+
+    const bubbles = {
+      xs: this.bubbliness * 7.5,
+      sm: this.bubbliness * 5,
+      md: this.bubbliness * 3,
+      lg: this.bubbliness * 2
+    };
+
+    Object.entries(bubbles).forEach(([size, count]) => {
+      this.createBubbles(count, `bubble-${size}`);
+    });
+
+    this.motionResistance *= -1; // Umkehren der Richtung für Bewegung
+  }
+
+  createBubbles(numberOfBubbles: number, className: string): void {
+    for (let i = 0; i < numberOfBubbles; i++) {
+      const bubble = this.renderer.createElement('div');
+      this.renderer.addClass(bubble, 'bubble');
+      this.renderer.addClass(bubble, className);
+      this.renderer.setStyle(bubble, 'top', `${Math.random() * 100}%`);
+      this.renderer.setStyle(bubble, 'left', `${Math.random() * 100}%`);
+      this.renderer.appendChild(this.layer, bubble);
+  
+      this.renderer.listen(bubble, 'click', (event) => this.onBubbleClick(event));
+      this.renderer.listen(bubble, 'mouseover', (event) => this.onBubbleMouseOver(event));
+    }
+  }
+
+  // @HostListener('window:mousemove', ['$event'])
+  // onMouseMove(event: MouseEvent): void {
+  //   if (window.innerWidth > 1024) {
+  //     const centerX = window.innerWidth / 2;
+  //     const centerY = window.innerHeight / 2;
+  //     const layerX = (event.clientX - centerX) / (this.motionResistance / 0.2);
+  //     const layerY = (event.clientY - centerY) / (this.motionResistance / 0.2);
+
+  //     this.renderer.setStyle(this.layer, 'left', `${layerX}px`);
+  //     this.renderer.setStyle(this.layer, 'top', `${layerY}px`);
+  //   }
+  // }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.initializeBubbles();
+  }
+
+  ngAfterViewInit(): void {
+    this.attachBubbleEvents();
+  }
+
+  private attachBubbleEvents(): void {
+    const isMobile = window.innerWidth <= 1024;
+    const bubbles = this.el.nativeElement.querySelectorAll('.bubble');
+
+    bubbles.forEach((bubble: HTMLElement) => {
+      if (isMobile) {
+        this.renderer.listen(bubble, 'click', this.onBubbleClick);
+      } else {
+        this.renderer.listen(bubble, 'mouseover', this.onBubbleMouseOver);
+      }
+    });
+  }
+
+  private onBubbleMouseOver(event: Event): void {
+    const bubble = event.target as HTMLElement;
+    if (bubble.classList.contains('bubble-wobble')) {
+      bubble.classList.add('bubble-pop');
+      setTimeout(() => (bubble.style.display = 'none'), 500);
+    } else {
+      bubble.classList.add('bubble-wobble');
+    }
+  }
+
+  private onBubbleClick(event: Event): void {
+    const bubble = event.target as HTMLElement;
+    bubble.classList.add('bubble-pop');
+    setTimeout(() => (bubble.style.display = 'none'), 500);
+  }
+}
--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -7,7 +7,8 @@ import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { CommentsComponent } from './comments/comments.component';
 import { ContactComponent } from './contact/contact.component';
- 
+import { BubbleEffectComponent } from '../main-content/bubbles/bubbles.component';
+
 @Component({
   selector: 'app-main-content',
   standalone: true,
@@ -20,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
     ProjectsComponent,
     CommentsComponent,
     ContactComponent,
+    BubbleEffectComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
@@ -27,16 +29,30 @@ import { ContactComponent } from './contact/contact.component';
 export class MainContentComponent implements AfterViewInit {
   private blubberContainer!: HTMLElement;
   private currentBlubberIndex = 0;
-  private maxBubbles = 12;
+  private maxBubbles = 8;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
-    this.blubberContainer = this.el.nativeElement.querySelector('.blubber-container');
+    this.blubberContainer =
+      this.el.nativeElement.querySelector('.blubber-container');
     if (!this.blubberContainer) return;
     this.createBubble();
+    this.checkBubbles();
   }
 
+  private setRandomSize(blubber: HTMLElement) {
+    const size = Math.random() * (180 - 40) + 60;
+    this.renderer.setStyle(blubber, 'width', `${size}px`);
+    this.renderer.setStyle(blubber, 'height', `${size}px`);
+  }
+
+  private setRandomPosition(blubber: HTMLElement) {
+    const x = Math.random() * (window.innerWidth - 220);
+    const y = Math.random() * (window.innerHeight - 120);
+    this.renderer.setStyle(blubber, 'left', `${x}px`);
+    this.renderer.setStyle(blubber, 'top', `${y}px`);
+  }
   private createBubble() {
     if (this.currentBlubberIndex >= this.maxBubbles) return;
 
@@ -46,6 +62,12 @@ export class MainContentComponent implements AfterViewInit {
     this.setRandomPosition(blubber);
 
     this.renderer.listen(blubber, 'click', () => this.burstBubble(blubber));
+    this.renderer.listen(blubber, 'mouseenter', () =>
+      this.onBubbleMouseOver(blubber)
+    );
+    this.renderer.listen(blubber, 'mouseleave', () =>
+      this.onBubbleMouseLeave(blubber)
+    );
 
     this.renderer.appendChild(this.blubberContainer, blubber);
     this.currentBlubberIndex++;
@@ -53,27 +75,99 @@ export class MainContentComponent implements AfterViewInit {
     setTimeout(() => this.createBubble(), 100);
   }
 
-  private setRandomSize(blubber: HTMLElement) {
-    const size = Math.random() * (180 - 80) + 60;
-    this.renderer.setStyle(blubber, 'width', `${size}px`);
-    this.renderer.setStyle(blubber, 'height', `${size}px`);
+
+  private burstBubble(blubber: HTMLElement) {
+    const width = parseFloat(blubber.style.width);
+    const height = parseFloat(blubber.style.height);
+    if (this.shouldRemoveBubble(width, height, blubber)) return;
+    const { originalX, originalY } = this.getBubblePosition(blubber);
+    this.removeBubble(blubber);
+    const newSize = this.calculateNewBubbleSize(width);
+    for (let i = 0; i < 2; i++) {
+      const newBubble = this.createNewBubble(newSize, originalX, originalY);
+      this.animateBubble(newBubble);
+    }
   }
-
-  private setRandomPosition(blubber: HTMLElement) {
-    const x = Math.random() * (window.innerWidth - 220);
-    const y = Math.random() * (window.innerHeight - 120);
-
-    this.renderer.setStyle(blubber, 'left', `${x}px`);
-    this.renderer.setStyle(blubber, 'top', `${y}px`);
+  
+  private shouldRemoveBubble(width: number, height: number, blubber: HTMLElement): boolean {
+    if (width < 60 || height < 60) {
+      this.removeBubble(blubber);
+      return true;
+    }
+    return false;
   }
-
-private burstBubble(blubber: HTMLElement) {
-  this.renderer.addClass(blubber, 'blubber.burst');
-
-  setTimeout(() => {
+  
+  private removeBubble(blubber: HTMLElement) {
     this.renderer.removeChild(this.blubberContainer, blubber);
-  }, 100);
-}
+    this.currentBlubberIndex--;
+  }
+  
+  private getBubblePosition(blubber: HTMLElement) {
+    const rect = blubber.getBoundingClientRect();
+    return { originalX: rect.left, originalY: rect.top };
+  }
+  
+  private calculateNewBubbleSize(currentSize: number): number {
+    return currentSize / 2;
+  }
+  
+  private createNewBubble(size: number, originalX: number, originalY: number): HTMLElement {
+    const newBubble = this.renderer.createElement('div');
+    this.renderer.addClass(newBubble, 'blubber');
+  
+    this.renderer.setStyle(newBubble, 'width', `${size}px`);
+    this.renderer.setStyle(newBubble, 'height', `${size}px`);
+  
+    const offsetX = (Math.random() - 0.5) * 60;
+    const offsetY = (Math.random() - 0.5) * 60;
+  
+    this.renderer.setStyle(newBubble, 'position', 'absolute');
+    this.renderer.setStyle(newBubble, 'left', `${originalX + offsetX}px`);
+    this.renderer.setStyle(newBubble, 'top', `${originalY + offsetY}px`);
+    this.renderer.setStyle(newBubble, 'transform', 'scale(0)');
+    this.renderer.setStyle(newBubble, 'transition', 'transform 0.3s ease-in-out');
+  
+    this.renderer.appendChild(this.blubberContainer, newBubble);
+    this.currentBlubberIndex++;
+  
+    this.renderer.listen(newBubble, 'click', () => this.burstBubble(newBubble));
+  
+    return newBubble;
+  }
+  
+  private animateBubble(newBubble: HTMLElement) {
+    setTimeout(() => {
+      this.renderer.setStyle(newBubble, 'transform', 'scale(1)');
+    }, 10);
+  }
+  
+
+  private onBubbleMouseOver(bubble: HTMLElement): void {
+    this.renderer.addClass(bubble, 'bubble-wobble');
+  }
+
+  private onBubbleMouseLeave(bubble: HTMLElement): void {
+    this.renderer.removeClass(bubble, 'bubble-wobble');
+  }
+
+  private checkBubbles() {
+    const bubbles = this.blubberContainer.querySelectorAll('.blubber');
+    bubbles.forEach((bubble) => {
+      const rect = bubble.getBoundingClientRect();
+      if (
+        rect.right < 0 ||
+        rect.left > window.innerWidth ||
+        rect.bottom < 0 ||
+        rect.top > window.innerHeight
+      ) {
+        this.renderer.removeChild(this.blubberContainer, bubble);
+        this.currentBlubberIndex--;
+        this.createBubble();
+      }
+    });
+
+    setTimeout(() => this.checkBubbles(), 500);
+  }
 
   ellipse = [
     {
@@ -85,9 +179,33 @@ private burstBubble(blubber: HTMLElement) {
     },
     { width: '206px', height: '206px', top: '90%', left: '700px', zIndex: -1 },
     { width: '82px', height: '82px', top: '936px', left: '1034px', zIndex: -1 },
-    { width: '108.75001525878906px', height: '108.75001525878906px', top: '2290px', left: '540px',zIndex: -1 },
-    { width: '183px', height: '183px', top: '2515px', left: '1134px', zIndex: -1 },
-    { width: '393.32px', height: '393.32px', top: '3000px', left: '1124px', zIndex: -1 },
-    { width: '393.3196119958484px', height: '393.3196119958484px', top: '2700px', left: '-194px', zIndex: -1 },
+    {
+      width: '108.75001525878906px',
+      height: '108.75001525878906px',
+      top: '2290px',
+      left: '540px',
+      zIndex: -1,
+    },
+    {
+      width: '183px',
+      height: '183px',
+      top: '2515px',
+      left: '1134px',
+      zIndex: -1,
+    },
+    {
+      width: '393.32px',
+      height: '393.32px',
+      top: '3000px',
+      left: '1124px',
+      zIndex: -1,
+    },
+    {
+      width: '393.3196119958484px',
+      height: '393.3196119958484px',
+      top: '2700px',
+      left: '-194px',
+      zIndex: -1,
+    },
   ];
 }
