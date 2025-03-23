e3c01d87296f2fe9faf6419802f7833064f571c1
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2025-03-23 14:48:24 +0100

    added translatemodul footer and jsdoc header&footer

--- portfolio/src/app/shared/components/footer/footer.component.ts
+++ portfolio/src/app/shared/components/footer/footer.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { fontWeightAnimation} from '../../../animations/animations';
+import { fontWeightAnimation } from '../../../animations/animations';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule,RouterLink, CommonModule],
+  imports: [RouterModule, RouterLink, CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  animations: [fontWeightAnimation]
+  animations: [fontWeightAnimation],
 })
 export class FooterComponent {
-
   hoverState: boolean = false;
   fontWeightState: string = 'normal';
 
+  /**
+   * Scrolls to a specific section on the page identified by the provided ID.
+   * This method performs a smooth scroll to the target element.
+   *
+   * @param {string} id - The ID of the target element to scroll to.
+   * @returns {void} - No return value.
+   */
   scrollToSection(id: string) {
     const element = document.getElementById(id);
     if (element) {
@@ -22,6 +29,12 @@ export class FooterComponent {
     }
   }
 
+  /**
+   * Toggles the hover state and sets the font weight based on the hover state.
+   *
+   * @param {boolean} state - The new hover state, indicating whether the mouse is over the element or not.
+   * @returns {void} - No return value.
+   */
   toggleHover(state: boolean) {
     this.hoverState = state;
     this.fontWeightState = state ? 'bold' : 'normal';
--- portfolio/src/app/shared/components/header/header.component.ts
+++ portfolio/src/app/shared/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
-import { fontWeightAnimation} from '../../../animations/animations';
+import { fontWeightAnimation } from '../../../animations/animations';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +9,22 @@ import { fontWeightAnimation} from '../../../animations/animations';
   imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  animations: [ fontWeightAnimation]
+  animations: [fontWeightAnimation],
 })
 export class HeaderComponent {
-
   hoverState: string = 'normal';
   fontWeightState: string = 'normal';
-  hoveredIndex: number | null = null; 
+  hoveredIndex: number | null = null;
 
   constructor(private translationService: TranslationService) {}
 
+  /**
+   * Scrolls to a specific section on the page identified by the provided ID.
+   * This method performs a smooth scroll to the target element.
+   *
+   * @param {string} id - The ID of the target element to scroll to.
+   * @returns {void} - No return value.
+   */
   scrollToSection(id: string) {
     const element = document.getElementById(id);
     if (element) {
@@ -26,17 +32,37 @@ export class HeaderComponent {
     }
   }
 
+  /**
+   * Switches the application's language by invoking the `changeLanguage` method from the TranslationService.
+   *
+   * @param {string} language - The language code to switch to (e.g., 'en', 'de').
+   * @returns {void} - No return value.
+   */
   switchLanguage(language: string): void {
     this.translationService.changeLanguage(language);
   }
 
+  /**
+   * Retrieves the current language used by the application.
+   * This value is fetched from the TranslationService.
+   *
+   * @returns {string} - The current language code (e.g., 'en', 'de').
+   */
   get currentLanguage(): string {
     return this.translationService.getCurrentLanguage();
   }
 
+  /**
+   * Toggles the hover state for an element, affecting both its hover style and font weight.
+   * Optionally, it sets the hovered index for further styling or functionality.
+   *
+   * @param {boolean} state - The new hover state, indicating whether the mouse is over the element.
+   * @param {number} [index] - The optional index to store when the element is hovered. Defaults to `null` if not provided.
+   * @returns {void} - No return value.
+   */
   toggleHover(state: boolean, index?: number) {
     this.hoverState = state ? 'hover' : 'normal';
     this.fontWeightState = state ? 'bold' : 'normal';
-    this.hoveredIndex = state ? index ?? null : null; 
+    this.hoveredIndex = state ? index ?? null : null;
   }
 }
